022c1cdd0bb2667162fcbc2ed7fb92d6d299e3c1
Arsenic <kubarodak2003@outlook.com>
2023-09-21 21:29:00 +0100

    homepage revamp!

---
+++ js/app.js
@@ -0,0 +1,56 @@
+let globalRainbowIndex = 0;
+
+let rainbowIndex = 
+[
+    "#fd7f7e",
+    "#ffc66d",
+    "#ffff61",
+    "#7bfa86",
+    "#73b0ff",
+    "#d97efd"
+];
+
+window.onload = () =>
+{
+    let rainbows = [...document.getElementsByClassName("rainbow")];
+
+    console.log(rainbows);
+
+    if(rainbows.length)
+        rainbows.forEach(e => 
+        {
+            e.innerHTML = getNextColor(e.innerHTML);
+        });
+    else
+        rainbows.innerHTML = getNextColor(rainbows.innerHTML);
+
+};
+
+function getNextColor(txt, global)
+{
+    let currentRainbowIndex = 0;
+    if(global)
+        currentRainbowIndex = globalRainbowIndex;
+
+    let arr = txt.split('');
+    let re = "";
+
+    arr.forEach(e => 
+    {
+        if(e == " ")
+        {
+            re += `<span style="white-space: pre;"> </span>`;
+            return;
+        }
+
+
+        re += `<span style="color:${rainbowIndex[(currentRainbowIndex++) % rainbowIndex.length]}">${e}</span>`;
+    });
+
+    return re;
+}
+
+function link(text)
+{
+    window.location.href = text;
+}
